1152a6df430ff8f212dcb508f3ff4a30b3417e87
Kareem Salman <kasalman17@gsb.columbia.edu>
2016-10-05 16:49:05 -0400

    Added about page

--- web.py
+++ web.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('sample_page.html')
+
+@app.route('/about')
+def second():
+	return render_template('about.html')
 
 app.run(debug=True)
